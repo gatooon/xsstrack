34a762d0802b871e9cc3fc1e841d3a8e3769f712
gatooon <gl.golitin@gmail.com>
2023-06-22 23:15:59 +0200

    "/" can be used on environment variable, CSS payloads can be used

--- servers/web/handle_functions.go
+++ servers/web/handle_functions.go
@@ -5,19 +5,31 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 type triggerFunc func(http.ResponseWriter, string, string)
 
-func specialUrl(writer http.ResponseWriter, request *http.Request, envVarName string, function triggerFunc, sendType string) {
-	fileName := os.Getenv(envVarName) + request.URL.String()
+func specialUrl(writer http.ResponseWriter, request *http.Request, envVarName string, function triggerFunc) {
+	fileName := strings.TrimSuffix(os.Getenv(envVarName), "/") + request.URL.String()
 	_, err := os.Stat(fileName)
+	fileExtension := filepath.Ext(fileName)
+	var mime string
+	switch fileExtension {
+	case ".js":
+		mime = "application/javascript"
+	case ".css":
+		mime = "text/css"
+	default:
+		mime = "text/html"
+	}
+
 	fmt.Println(fileName)
 	if os.IsNotExist(err) {
 		fmt.Println("payload not sent")
 	} else {
-		function(writer, fileName, sendType)
+		function(writer, fileName, mime)
 		fmt.Println(request.URL.String() + " sent")
 	}
 }
@@ -25,9 +37,9 @@ func specialUrl(writer http.ResponseWriter, request *http.Request, envVarName st
 func catch(writer http.ResponseWriter, request *http.Request) {
 	switch {
 	case strings.HasPrefix(request.URL.String(), "/payloads/"):
-		specialUrl(writer, request, "XSSTRACK_PAYLOADS_FOLDER", sendPayload, "application/javascript")
+		specialUrl(writer, request, "XSSTRACK_PAYLOADS_FOLDER", sendPayload)
 	case strings.HasPrefix(request.URL.String(), "/web/"):
-		specialUrl(writer, request, "XSSTRACK_WEB_FOLDER", sendPayload, "text/html")
+		specialUrl(writer, request, "XSSTRACK_WEB_FOLDER", sendPayload)
 	}
 
 	RequestData.Url = request.URL.String()
@@ -41,7 +53,7 @@ func catch(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println(request.URL.String())
 }
 
-func sendPayload(writer http.ResponseWriter, fileName string, sendType string) {
+func sendPayload(writer http.ResponseWriter, fileName string, mime string) {
 
 	data, _ := os.Open(fileName)
 
@@ -52,7 +64,7 @@ func sendPayload(writer http.ResponseWriter, fileName string, sendType string) {
 	buffer := make([]byte, dataSize)
 	data.Read(buffer)
 
-	writer.Header().Set("Content-Type", sendType)
+	writer.Header().Set("Content-Type", mime)
 	writer.Header().Set("Access-Control-Allow-Origin", "*")
 	writer.Write(buffer)
 
